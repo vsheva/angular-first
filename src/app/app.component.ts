ac4385cdd8b07d10783a073e7667f06cd846d535
vsheva <valera.sheva@gmail.com>
2023-11-04 22:59:12 -0400

    corrections

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
   showContent = false;
-  log = [1];
+  log = [];
 
   onToggle() {
     this.showContent = !this.showContent;
